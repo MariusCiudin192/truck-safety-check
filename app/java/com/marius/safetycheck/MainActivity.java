2c1211124f6cdf3db5405f4aec4d5eecdc4ee633
MariusCiudin192 <24156112@ardenuniversity.ac.uk>
2025-08-10 13:56:20 +0100

    Create MainActivity.java

---
+++ app/java/com/marius/safetycheck/MainActivity.java
@@ -0,0 +1,36 @@
+package com.marius.safetycheck;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Schimbă link-ul cu cel pe care vrei să-l încarce aplicația
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://truck-safety-check.example.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
